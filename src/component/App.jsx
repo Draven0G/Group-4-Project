a0532f0c262462c7bf0303dd13820059383839b6
joy <blackbearmode1@gmail.com>
2024-05-07 20:35:22 +0300

    complete

---
+++ src/component/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "./component/Navbar/Navbar";
+
+const App =() => {
+    return (
+        <div className='app'>
+        </div>
+    )
+}
+
+export default App;
